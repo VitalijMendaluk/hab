30f3cb508fb419781150db53a94dbd7275f4c525
VitalijMendaluk <mend.vitalij@gmail.com>
2026-08-05 14:14:46 +0200

    Add files via upload

--- config/links.ts
+++ config/links.ts
@@ -12,7 +12,7 @@ export const LINKS = {
   BONUS_4_CAMERA_FEAR: "https://strahcamery.vercel.app/",
   BONUS_5_SALES_SCRIPT: "https://drive.google.com/drive/folders/1JoaV1OOU_7o1fhQktFjTLBBHikOI4oV6?hl=ru",
   BONUS_6_PRESENTATION: "https://canva.link/cridweqsffx1nez",
-  BONUS_7_AI_PROMPTS: "PLACEHOLDER",
+  BONUS_7_AI_PROMPTS: "https://drive.google.com/file/d/1pBvh7Y7z1hu24fW6pPrSqDSwgkH5gxJf/view?usp=sharing",
   BONUS_8_CHECKLIST: "https://drive.google.com/file/d/1k5mjtzCHXSnMFhLCsfGuo-iCO-K_Kf_n/view?usp=sharing",
   BONUS_9_HOOKS: "https://drive.google.com/file/d/1kRBlkOkmMQ1YL587R8osG9A1uf4TD1l-/view?usp=sharing",
   BONUS_10_MISTAKES: "https://drive.google.com/file/d/1UlrD6mdA1MxciWa8Vh6hm1cmrdNKr9_Q/view?usp=sharing",
